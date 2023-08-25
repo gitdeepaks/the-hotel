194a6f95e30f95993dcca2d0c23241b1a944acd2
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-25 21:07:28 +0530

    new commmit 🚀

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,17 +2,26 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import Button from "../ui/Button";
+import { useState } from "react";
+// import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowform] = useState(false);
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>TEST</p>
+        <p>FILTER / SORT</p>
       </Row>
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowform((show) => !show)}>
+          Add new Cabin
+        </Button>
+        {/* {showForm && <CreateCabinForm />} */}
       </Row>
     </>
   );
