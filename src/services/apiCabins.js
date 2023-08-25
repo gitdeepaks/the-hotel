6b4aad9b22c3a91283c40265fa24c285bf5cdc24
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-25 16:39:01 +0530

    new commmit 🚀

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,13 @@ export async function getCabins() {
 
   return data;
 }
+
+export const deleteCabin = async (id) => {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error("error deleting cabin");
+    throw new Error("cabin could not be deleted from database");
+  }
+  return data;
+};
